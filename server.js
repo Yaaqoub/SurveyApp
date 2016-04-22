a69ef002d096e3822e8eff3ef3190c8b5ff925ff
Stephanie Wang <steph17@stanford.edu>
2016-04-21 23:51:21 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var router = require('./routes');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var uuid = require('uuid');
+var session = require('express-session');
+
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+	genid: function(req){
+		return uuid.v1();
+	},
+	secret: 'steph17',
+	cookie:{ secure: false },
+	resave: false,
+	saveUninitialized: false
+}));
+app.use('/', router);
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/routes" + "/index.jade" );
+});
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+    });
